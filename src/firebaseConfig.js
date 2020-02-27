5c467d72f2855018e47ceeaeb95de0c4f9835792
annin401 <annin401@gmail.com>
2020-02-27 23:31:37 +0900

    chore: firebaseの構成情報を追加

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAB8WlX43Inp7hKG2tOAcN7zXHvv4h4zd8',
+  authDomain: 'lask-58.firebaseapp.com',
+  databaseURL: 'https://lask-58.firebaseio.com',
+  projectId: 'lask-58',
+  storageBucket: 'lask-58.appspot.com',
+  messagingSenderId: '845410359226',
+  appId: '1:845410359226:web:03061e097646003014ef83',
+  measurementId: 'G-TR52PW2L4T'
+};
+
+export default firebaseConfig;
